40bdae403df3b428e53d00d2e300b6ce9e5f3eab
Kevin Souza <kevinwillyansouza@gmail.com>
2024-01-15 17:39:31 -0400

    feat: adição da integração com a API do YouTube

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -5,58 +5,105 @@ let videoFundo = document.getElementById("backgroundVideo");
 let telaLoading = document.querySelector(".tela_loading");
 const btnProximo = document.querySelector("#btnProximo");
 
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 let data = [
-    "GvHiffqm5ic",
-    "_EGPF4lOhS0",
-    "3crVz-PScY0",
-    "z4iKx2EB6yI",
-    "X1p_nufU6Jk",
     "z_V5qk91x84",
     "Y1l3ePucj1c",
-    "tbGLhR8-h3k",
+    "X1p_nufU6Jk",
     "8mEy0RjAURQ",
     "mqr_HZoigf8",
 ];
 
 let ultimoSorteado = -1;
+let currentVideoIndex = sortearDiferenteAnterior(data);
+let canPlay = false;
+let playerClickedBefore = false;
+let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+        height: 360,
+        width: 640,
+        videoId: data[currentVideoIndex],
+        playerVars: {
+            autoplay: 0,
+            controls: 0,
+            showinfo: 0,
+            rel: 0,
+            fs: 1,
+            modestbranding: 1,
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange,
+        },
+    });
+}
+
+function onPlayerReady() {
+    canPlay = true;
+
+    if (playerClickedBefore) {
+        playVideo();
+    }
+}
+
+function onPlayerStateChange(event) {
+    document.querySelector("#player").classList.remove("player-hidden");
+    document
+        .querySelector(".tela_loading")
+        .classList.add("tela_loading-Hidden");
+
+    if (event.data == YT.PlayerState.ENDED) {
+        loadNextVideo();
+    }
+}
+
+function loadNextVideo() {
+    player.loadVideoById(data[sortearDiferenteAnterior(data)]);
+}
+
+function playVideo() {
+    if (player.getPlayerState() !== YT.PlayerState.PLAYING) {
+        player.playVideo();
+    }
+}
+
+function setQuality(quality) {
+    player.setPlaybackQuality(quality);
+}
 
 btnProximo.addEventListener("click", () => {
-    nextVideo();
+    loadNextVideo();
 });
 
 buttonAcept.addEventListener("click", () => {
+    playerClickedBefore = true;
     telaBemVindo.remove();
-    ultimoSorteado = sortearDiferenteAnterior(data, ultimoSorteado);
-    videoFundo.src = data[ultimoSorteado];
-    telaLoading.classList.remove("tela_loading-Hidden");
+    document.querySelector(".cover").style.display = "block";
+    document
+        .querySelector(".container-proximo")
+        .classList.remove("tela_loading-Hidden");
+    document
+        .querySelector(".tela_loading")
+        .classList.remove("tela_loading-Hidden");
+    if (canPlay) {
+        playVideo();
+    }
 });
 
-videoFundo.addEventListener("loadeddata", () => {
-    setTimeout(() => {
-        telaLoading.classList.add("tela_loading-Hidden");
-        videoFundo.play();
-        gerenciaVideos();
-        document
-            .querySelector(".container-proximo")
-            .classList.remove("tela_loading-Hidden");
-    }, 2000);
-});
-
-function nextVideo() {
-    ultimoSorteado = sortearDiferenteAnterior(data, ultimoSorteado);
-    videoFundo.src = data[ultimoSorteado];
-    videoFundo.play();
-    gerenciaVideos();
-}
-function gerenciaVideos() {
-    videoFundo.addEventListener("ended", nextVideo);
-}
-
-function sortearDiferenteAnterior(array, ultimoSorteado) {
+function sortearDiferenteAnterior(array) {
     let novoSorteado;
     do {
         novoSorteado = Math.floor(Math.random() * array.length);
     } while (novoSorteado === ultimoSorteado);
+
+    ultimoSorteado = novoSorteado;
 
     return novoSorteado;
 }
